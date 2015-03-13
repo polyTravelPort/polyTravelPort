6b9ba8f059124855424e7f5ada8a4f58f9a0c74e
polytravelport <tammy_herndon@comcast.net>
2015-03-13 20:13:47 +0000

    adding code

--- js/global.js
+++ js/global.js
@@ -11,9 +11,37 @@ document.addEventListener('polymer-ready', function() {
  function handleFavorite ()
  {
     $(function(){
-      alert("me");
-      $("#changePanel").load("b.html"); 
+      $("#changePanel").load("tcard.html"); 
     });
- }
+ };
+ 
+ 	document.querySelector('#cards').addEventListener('swipeable-card-swipe-away', function(e)
+	{
+//		console.log( document.querySelector('#cards') );
+//		console.log( e.target.parentNode );
+//		console.log( document.querySelector('#selectedCards') );
+//		var selectedCards = document.querySelector('#selectedCards');
+//		var cards = document.querySelector('#cards');
+
+//		cards.removeChild(e.target);
+		e.target.parentNode.removeChild(e.target);
+//		selectedCards.appendChild( e.target );
+	});
+
+	function decreaseShadow()
+	{
+		var card = document.querySelector('#shadow_demo');
+		var z = parseInt(card.getAttribute('z'));
+		z = z > 0 ? z - 1 : 0;
+		card.setAttribute('z', z);
+	}
+
+	function increaseShadow()
+	{
+		var card = document.querySelector('#shadow_demo');
+		var z = parseInt(card.getAttribute('z'));
+		z = z < 5 ? z + 1 : 5;
+		card.setAttribute('z', z);
+	}
  
 
